14b951fdff6b3192a71af4708d228d98f2902ddd
Anna Nebuko <Anna_Nebuko@epam.com>
2022-07-02 10:38:35 +0300

    liquibase basic config

---
+++ src/com/Aniken/Main.java
@@ -0,0 +1,10 @@
+package com.Aniken;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(
+            "Liquibase -  project that helps manage database schema changes"
+        );
+    }
+}
